add1fb09f15d9f06aa52537b20b949f4659c6493
Nick Karpenske <randland@gmail.com>
2020-12-03 02:34:06 -0700

    :art: Add recursive solution to Day 1

--- day-01/ruby/main.rb
+++ day-01/ruby/main.rb
@@ -3,23 +3,45 @@
 INPUT = File.read(ARGV[0])
 DATA = INPUT.split("\n").map(&:to_i)
 
-def part_1(target)
-  DATA[0..-2].each_with_index do |a, idx|
-    DATA[idx+1..-1].each do |b|
-      return "#{a * b} (#{a}, #{b})" if a + b == target
+def iterative_find_2(array, target)
+  array[0 .. -2].each_with_index do |a, idx|
+    array[idx + 1 .. -1].each do |b|
+      return [a, b] if a + b == target
     end
   end
 end
 
-def part_2(target)
-  DATA[0..-3].each.with_index do |a, idx_a|
-    DATA[idx_a+1..-2].each.with_index do |b, idx_b|
-      DATA[idx_a+idx_b+2..-1].each do |c|
-        return "#{a * b * c} (#{a}, #{b}, #{c})" if a + b + c == target
+def iterative_find_3(array, target)
+  array[0 .. -3].each.with_index do |a, idx_a|
+    array[idx_a + 1 .. -2].each.with_index do |b, idx_b|
+      array[idx_a + idx_b + 2 .. -1].each do |c|
+        return [a, b, c] if a + b + c == target
       end
     end
   end
 end
 
-puts "Part 1: #{part_1(2020)}"
-puts "Part 2: #{part_2(2020)}"
+def recursive_find(array, target, remaining)
+  if remaining == 1
+    return array.include?(target) ? [target] : nil
+  end
+
+  array.each_with_index do |num, idx|
+    next if num > target
+
+    found = recursive_find(array[idx + 1 .. -1], target - num, remaining - 1)
+    return found.unshift(num) unless found.nil?
+  end
+
+  nil
+end
+
+def result_display(part_num, note, vals)
+  "Part #{part_num} (#{note}): #{vals.inject(:*)} (#{vals.join(", ")})"
+end
+
+puts result_display(1, "iterative", iterative_find_2(DATA, 2020))
+puts result_display(1, "recursive", recursive_find(DATA, 2020, 2))
+puts
+puts result_display(2, "iterative", iterative_find_3(DATA, 2020))
+puts result_display(2, "recursive", recursive_find(DATA, 2020, 3))
