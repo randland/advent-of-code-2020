e785dc868e37f464f73bb545a1049264450c5487
Nick Karpenske <randland@gmail.com>
2020-12-08 23:07:42 -0700

    :art: Lower par

--- day-09/ruby/main.rb
+++ day-09/ruby/main.rb
@@ -1,30 +1,27 @@
 # frozen_string_literal: true
 
 INPUT = File.read(ARGV[0])
-DATA = INPUT.split("\n")
+DATA = INPUT.split("\n").map(&:to_i)
 
-def first_invalid_num(list, block_size)
-  (block_size...list.size).each do |idx|
-    block_start = idx - block_size
-    sums = list[block_start, block_size].combination(2).map { |pair| pair.inject(:+) }
+def first_invalid_num(list, preamble)
+  (preamble...list.size).each do |idx|
+    sums = list[idx - preamble, preamble].combination(2).map(&:sum)
     return list[idx] unless sums.include?(list[idx])
   end
 end
 
 def part_1(data)
-  first_invalid_num(data.map(&:to_i), 25)
+  first_invalid_num(data, 25)
 end
 
 def find_contig_list(list, target)
-  idx_a, idx_b = 0, 0
-  while list[idx_a..idx_b].inject(:+) != target
-    list[idx_a..idx_b].inject(:+) < target ? idx_b += 1 : idx_a += 1
-  end
-  list[idx_a..idx_b]
+  a, b = 0, 0
+  list[a..b].sum > target ? a += 1 : b += 1 while list[a..b].sum != target
+  list[a..b]
 end
 
 def part_2(data)
-  find_contig_list(data.map(&:to_i), part_1(data)).tap { |result| return result.min + result.max }
+  find_contig_list(data, part_1(data)).tap { |result| return result.min + result.max }
 end
 
 puts "Part 1: #{part_1(DATA)}"
