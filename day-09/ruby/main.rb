9e68fc144aab0346a9619bf19942193c4d7f78d8
Nick Karpenske <randland@gmail.com>
2020-12-08 22:49:17 -0700

    :art: Lower par

--- day-09/ruby/main.rb
+++ day-09/ruby/main.rb
@@ -3,13 +3,10 @@
 INPUT = File.read(ARGV[0])
 DATA = INPUT.split("\n")
 
-def contig_list(list, idx, size)
-  list[idx...idx+size]
-end
-
 def first_invalid_num(list, block_size)
   (block_size...list.size).each do |idx|
-    sums = list[idx-block_size...idx].combination(2).map { |pair| pair.inject(:+) }
+    block_start = idx - block_size
+    sums = list[block_start, block_size].combination(2).map { |pair| pair.inject(:+) }
     return list[idx] unless sums.include?(list[idx])
   end
 end
@@ -23,14 +20,14 @@ def find_contig_list(list, target)
     size = 1
     begin 
       size += 1
-      sublist = list[idx...idx+size]
+      sublist = list[idx, size]
     end while sublist.inject(:+) < target
     return sublist if sublist.inject(:+) == target
   end
 end
 
 def part_2(data)
-  find_contig_list(data.map(:to_i), part_1(data)).tap { |result| return result.min + result.max }
+  find_contig_list(data.map(&:to_i), part_1(data)).tap { |result| return result.min + result.max }
 end
 
 puts "Part 1: #{part_1(DATA)}"
