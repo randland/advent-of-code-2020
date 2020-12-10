230a5e28bf3462a5951d55111465c5abff0089b6
Nick Karpenske <randland@gmail.com>
2020-12-10 00:42:05 -0700

    :art: Easier to read

--- day-10/ruby/main.rb
+++ day-10/ruby/main.rb
@@ -3,20 +3,26 @@
 INPUT = File.read(ARGV[0])
 DATA = INPUT.split("\n").map(&:to_i)
 
+MAX_GAP = 3
+
 def full_list(list)
   sorted_list = list.sort
-  [0] + sorted_list + [sorted_list.last + 3]
+
+  [0] + sorted_list + [sorted_list.last + MAX_GAP]
 end
 
 def part_1(data)
   counts = Hash.new { 0 }
-  pairs = full_list(data).each_cons(2)
-  pairs.each { |a, b| counts[b - a] += 1 }
+  contig_pairs = full_list(data).each_cons(2)
+  contig_pairs.each { |a, b| counts[b - a] += 1 }
+
   counts[1] * counts[3]
 end
 
 def valid_permutation?(list)
-  list.each_cons(2).all? { |a, b| (1..3).include?(b - a) }
+  contig_pairs = list.each_cons(2)
+
+  contig_pairs.all? { |a, b| (1..MAX_GAP).include?(b - a) }
 end
 
 def valid_permutations_count(list)
@@ -25,16 +31,21 @@ def valid_permutations_count(list)
   mid_list = list[1..-2]
   perm_count = 2 ** mid_list.size
 
-  perm_count.times.count do |perm_code|
-    kept_nums = perm_code.to_s(2).chars.reverse.map { |c| c == "1" }
+  perm_count.times.count do |perm_idx|
+    perm_binary = perm_idx.to_s(2)
+    kept_nums = perm_binary.chars.reverse.map { |c| c == "1" }
     mid_perm = mid_list.select.with_index { |_, idx| kept_nums[idx] }
+
     valid_permutation?([list.first] + mid_perm + [list.last])
   end
 end
 
 def part_2(data)
-  partitions = full_list(data).slice_when { |a, b| b - a == 3 }
-  partitions.map(&method(:valid_permutations_count)).inject(:*)
+  list_partitioned_by_max_gap = full_list(data).slice_when { |a, b| b - a == MAX_GAP }
+
+  list_partitioned_by_max_gap.map do |sublist|
+    valid_permutations_count(sublist)
+  end.inject(:*)
 end
 
 puts "Part 1: #{part_1(DATA)}"
