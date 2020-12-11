28ef9b8c6a50530ab6fcffc89230480ca3ad9b81
Nick Karpenske <randland@gmail.com>
2020-12-10 17:15:03 -0700

    :art: Move params to constants

--- day-04/ruby/main.rb
+++ day-04/ruby/main.rb
@@ -7,6 +7,18 @@ def parse_passports(data)
   data.map { |row| Hash[row.split(/\s/).map { |tuple| tuple.split(":") }] }
 end
 
+PART_1_FIELDS = %w{byr iyr eyr hgt hcl ecl pid}
+PART_2_CONSTRAINTS = {
+  byr: ->(byr) { byr.length == 4 && (1920..2002).include?(byr.to_i) },
+  iyr: ->(iyr) { iyr.length == 4 && (2010..2020).include?(iyr.to_i) },
+  eyr: ->(eyr) { eyr.length == 4 && (2020..2030).include?(eyr.to_i) },
+  hgt: ->(hgt) { hgt =~ /\A\d{2,3}(cm|in)\Z/ &&
+                 ($1 == "cm" ? (150..193) : (59..76)).include?(hgt.to_i) },
+  hcl: ->(hcl) { hcl =~ /\A#[\da-f]{6}\Z/ },
+  ecl: ->(ecl) { ecl =~ /\A(amb|blu|brn|gry|grn|hzl|oth)\Z/ },
+  pid: ->(pid) { pid =~ /\A\d{9}\Z/ }
+}.transform_keys(&:to_s)
+
 def part_1(data, required_fields)
   parse_passports(data).count { |passport| (required_fields - passport.keys).empty? }
 end
@@ -19,17 +31,5 @@ def part_2(data, field_requirements)
   end
 end
 
-part_1_fields = %w{byr iyr eyr hgt hcl ecl pid}
-puts "Part 1: #{part_1(DATA, part_1_fields)}"
-
-part_2_constraints = {
-  byr: ->(byr) { byr.length == 4 && (1920..2002).include?(byr.to_i) },
-  iyr: ->(iyr) { iyr.length == 4 && (2010..2020).include?(iyr.to_i) },
-  eyr: ->(eyr) { eyr.length == 4 && (2020..2030).include?(eyr.to_i) },
-  hgt: ->(hgt) { hgt =~ /\A\d{2,3}(cm|in)\Z/ &&
-                 ($1 == "cm" ? (150..193) : (59..76)).include?(hgt.to_i) },
-  hcl: ->(hcl) { hcl =~ /\A#[\da-f]{6}\Z/ },
-  ecl: ->(ecl) { ecl =~ /\A(amb|blu|brn|gry|grn|hzl|oth)\Z/ },
-  pid: ->(pid) { pid =~ /\A\d{9}\Z/ }
-}.transform_keys(&:to_s)
-puts "Part 2: #{part_2(DATA, part_2_constraints)}"
+puts "Part 1: #{part_1(DATA, PART_1_FIELDS)}"
+puts "Part 2: #{part_2(DATA, PART_2_CONSTRAINTS)}"
